e070f490de2f02afa93126797444016e9a8b3543
Mykhailo Loginov <mihich@Mykhailos-MacBook-Pro.local>
2016-04-18 22:30:55 +0100

    Modified to v2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello from mihich");
+  response.end("Hello from mihich v2");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
