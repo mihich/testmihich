ea4ea13eb21d0912373ed24ffbb4f97f29db048e
Mykhailo Loginov <mihich@Mykhailos-MacBook-Pro.local>
2016-04-19 12:08:04 +0100

    Added rc file for kubernetes replicator controller, updated to v0.4

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello from mihich v3");
+  response.end("Hello from mihich v4.0 release");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
