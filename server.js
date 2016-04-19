d13b27e212d6243a015482bb18e4b4ecde48d988
Mykhailo Loginov <mihich@Mykhailos-MacBook-Pro.local>
2016-04-19 12:23:52 +0100

    Changed to 0.4.2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello from mihich v4.0 release");
+  response.end("Hello from mihich v0.4.2 release");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
